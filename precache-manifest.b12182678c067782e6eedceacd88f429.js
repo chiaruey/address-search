0d169a5381ac91d625a7b38e0cf71b8ada83ad18
chiaruey <chiaruey@gmail.com>
2020-04-13 11:18:29 -0700

    Updates

---
+++ precache-manifest.b12182678c067782e6eedceacd88f429.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d516e7303f3a91dcd45498c82193a2",
+    "url": "/address-search/index.html"
+  },
+  {
+    "revision": "add4ad598ed4a8de66c4",
+    "url": "/address-search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "29985bb69b5794bfc913",
+    "url": "/address-search/static/js/2.32701790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/address-search/static/js/2.32701790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add4ad598ed4a8de66c4",
+    "url": "/address-search/static/js/main.f356cf48.chunk.js"
+  },
+  {
+    "revision": "b2b3fe99d249be5076da",
+    "url": "/address-search/static/js/runtime-main.b98821d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/address-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
